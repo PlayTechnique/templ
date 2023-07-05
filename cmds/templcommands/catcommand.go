e1bf2534a31a584f1cde119b49866671b567d9c3
Gwyn <gwyn@playtechnique.io>
2023-07-04 21:51:25 -0600

    Now I no longer use relative paths in the codebase, convert cat to abspaths. (#2)

--- cmds/templcommands/catcommand.go
+++ cmds/templcommands/catcommand.go
@@ -7,6 +7,7 @@ import (
 	"github.com/google/subcommands"
 	"github.com/sirupsen/logrus"
 	"os"
+	"path/filepath"
 )
 
 type CatCommand struct {
@@ -45,15 +46,25 @@ func (c CatCommand) SetFlags(_ *flag.FlagSet) {
 
 }
 
-func (CatCommand) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+func (c CatCommand) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
 
 	logrus.Debug(f)
-	catArgs := makeSet(f.Args())
+	// If a file name is supplied twice, only have it once in the data structure. This keeps the cat output
+	// clean, rather than having duplicated files.
+	// The key in the set is the relative path to the file from the os.args array. The value in the set is
+	// empty.
+	absPaths, err := getAbsPaths(f.Args(), c.templatedirectory)
+	catArgs := makeSet(absPaths)
+	if err != nil {
+		logrus.Error(err)
+		return subcommands.ExitFailure
+	}
+
 	logrus.Debug("catArgs: ", catArgs)
 
 	//For each file named in the args to cat, search the current working directory to see if it exists
 	// TODO: change the signature of findFilesByName to take a slice of strings
-	matchingfiles, err := findFilesByName(".", catArgs)
+	matchingfiles, err := findFilesByName(c.templatedirectory, catArgs)
 	logrus.Debug("Found files: ", matchingfiles)
 
 	if err != nil {
@@ -85,4 +96,20 @@ func (CatCommand) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) 
 func NewCatCommand(templatedirectory string) CatCommand {
 	catcommand.templatedirectory = templatedirectory
 	return catcommand
+}
+
+func getAbsPaths(paths []string, root string) ([]string, error) {
+	var absPaths = make([]string, 0)
+
+	for _, path := range paths {
+		absPath, err := filepath.Abs(root + "/" + path)
+
+		if err != nil {
+			return absPaths, err
+		}
+
+		absPaths = append(absPaths, absPath)
+	}
+
+	return absPaths, nil
 }
