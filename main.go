ba79fc3711a50f7b035211268217d208ac79be3b
Gwyn <gwyn@playtechnique.io>
2023-08-22 21:25:10 -0600

    refactoring bigly
    - removes the dependency on subcommands. It's too heavyweight; this
    tool can exist using the standard library to manage arguments.
    
    - reimplements the repository functionality using simple  combinations
    of types and an interface. There are constructors to return correctly
    initialised instances of two repository types. First, the github repository,
    and second it now excitingly supports local git repositories. This
    really makes types of repository much more extensible; the old implementation
    really was going to be hard to add new versions of.
    
    - removes the cmds subdirectory. There are portions of the old
    implementation of the logic that I dig and will resurrect, but it was not
    very go-ish.
    
    - configelements is a package to track configuration elements. I
    can make it more generic, but it just tracks an environment variable
    name to store config elements, a default and a way to set the value
    to something new.
    
    - pretty decent test coverage! The first version really hasn't ever
    really had this.

--- main.go
+++ main.go
@@ -1,120 +1,32 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"fmt"
-	"github.com/google/subcommands"
-	"github.com/gwynforthewyn/templ/cmds/templcommands"
-	"github.com/sirupsen/logrus"
-	"io"
-	"os"
-	"path/filepath"
+	"templ/repository"
 )
 
-func init() {
-	debugger, ok := os.LookupEnv("TEMPL_DEBUG_BREAK")
-
-	if ok && debugger != "" {
-		fmt.Println("Attach goland debugger to PID: ", os.Getpid())
-		fmt.Println("Press enter to continue")
-		fmt.Scanln()
-
-	}
-
-	lvl, ok := os.LookupEnv("TEMPL_LOG_LEVEL")
-	// LOG_LEVEL not set, let's default to debug
-	if !ok {
-		lvl = "warn"
-	}
-
-	// parse string, this is built-in feature of logrus
-	ll, err := logrus.ParseLevel(lvl)
-	if err != nil {
-		ll = logrus.DebugLevel
-	}
-	// set file names and line number to appear in log messages
-	logrus.SetReportCaller(true)
-
-	// set global log level
-	logrus.SetLevel(ll)
-
-}
-
-// Main identifies the templates directory, switches working directories into it and invokes the subcommand.
 func main() {
-	ctx := context.Background()
-	tflags := flag.NewFlagSet("templFlags", flag.ExitOnError)
+	// string flag called clone. Takes a url as an argument
+	url := flag.String("clone", "", "clone a repo from a url. Can be a github url or a local git repository.")
 
-	templCommander := subcommands.NewCommander(tflags, os.Args[0])
+	flag.Parse()
 
-	logrus.Info("Starting ", os.Args[0])
+	// I use github exclusively right now, so this is a safe bet. If I need to support more version control systems
+	// I'll need to implement a case statement and switch on some piece of data to figure out the difference between
+	// repo types. Probably need an extra cli flag.
 
-	templatesDir, err := getTemplatesDirectory()
-	if err != nil {
-		panic(err)
-	}
+	// If the user has provided a url, clone the repo
+	if *url != "" {
+		templRepo, err := repository.NewGitRepository(*url)
 
-	//Super cheap way to get documentation into the usage message.
-	oldHelp := templCommander.Explain
-	help := func(w io.Writer) {
-		fmt.Fprintf(w, "Env Vars: TEMPL_LOG_LEVEL TEMPL_DIR TEMPL_DEBUG_BREAK\n")
-		oldHelp(w)
-
-	}
-
-	templCommander.Explain = help
-
-	templCommander.Register(subcommands.HelpCommand(), "help")
-	templCommander.Register(templcommands.NewCatCommand(templatesDir), "templates")
-	templCommander.Register(templcommands.NewListCommand(templatesDir), "templates")
-	templCommander.Register(subcommands.Alias("ls", templcommands.NewListCommand(templatesDir)), "templates")
-	templCommander.Register(templcommands.NewRepoCommand(templatesDir), "templates")
-	templCommander.Register(templcommands.NewRenderCommand(templatesDir), "templates")
-
-	tflags.Parse(os.Args[1:])
-
-	exitval := int(templCommander.Execute(ctx, os.Args))
-	os.Exit(exitval)
-}
-
-// Interrogates the environment variable TEMPL_DIR for a directory of templates.
-// If that variable does not exist, it attempts to switch into the default template directory.
-func getTemplatesDirectory() (string, error) {
-
-	templatesDir, foundEnvVar := os.LookupEnv("TEMPL_DIR")
-
-	// Only auto-create TEMPL_DIR if we're using the default directory. The reasoning is that if
-	// the user explicitly provides the directory to use then they're expected to have already
-	// created it. If not, then we are make sure the default setup is correct.
-	if foundEnvVar {
-		logrus.Debug("Found TEMPL_DIR=", templatesDir)
-	} else {
-		logrus.Debug("Did not find TEMPL_DIR. Switching to default templates directory.")
-		home, foundHome := os.LookupEnv("HOME")
-
-		if !foundHome {
-			logrus.Error("HOME env var not found. templ cannot autodiscover the default templates dir, and TEMPL_DIR is not set.")
-			panic("Please review the help for preconditions to meet running templ/")
+		if err != nil {
+			panic(err)
 		}
 
-		templatesDir = home + "/.config/templ"
+		err = templRepo.Fetch()
 
-		if _, err := os.Stat(templatesDir); err == nil || os.IsNotExist(err) {
-			logrus.Info("Did not find <" + templatesDir + "> directory. Creating...")
-			err := os.MkdirAll(templatesDir, 0700)
-
-			if err != nil {
-				panic(err)
-			}
+		if err != nil {
+			panic(err)
 		}
 	}
-
-	templatesDir, err := filepath.Abs(templatesDir)
-	if err != nil {
-		err = fmt.Errorf("something wrong with %s: %v", templatesDir, err)
-	}
-
-	return templatesDir, err
-
 }
---
+++ repository/repository.go
@@ -0,0 +1,184 @@
+package repository
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"regexp"
+	"templ/configelements"
+)
+
+// Errors
+type ErrEmptyUpstream struct{}
+
+func (e ErrEmptyUpstream) Error() string {
+	return "no upstream provided"
+}
+
+type ErrInvalidUpstream struct{}
+
+func (e ErrInvalidUpstream) Error() string {
+	return "invalid upstream: does not match expected patterns."
+}
+
+type ErrNotDirectory struct{}
+
+func (e ErrNotDirectory) Error() string {
+	return "not a directory"
+}
+
+// Abstract behaviours
+// The Repository type implements the CommonRepositoryBehaviour interface.
+type CommonRepositoryBehaviour interface {
+	//TemplDestination returns the location on the file system the repository is intended for.
+	TemplDestination() string
+	// Fetch performs a git clone.
+	Fetch() error
+	//Origin returns the origin you initialised the repository with.
+	Origin() string
+}
+
+type Repository struct {
+	//The directory that the repository will be cloned to. The taxonomy is:
+	//${TEMPL_DIR}/<repo type>/<repo identifiers, can be multiple directories>/
+	Destination string
+	//The location we clone from
+	Upstream string
+	RepoType string
+	//Validation patterns for the upstream. We also use data from the upstream to generate the destination
+	ValidUpstreams []string
+}
+
+// NewGitRepository handles figuring out which of the types of git repository we're dealing with and returns
+// the appropriate concrete type.
+// This constructor and the NewLocalGitRepository structure both benefit from checking os.Stat on the upstream string.
+// but they have different purposes. Here, we're trying to determine the right repository type; in NewLocalGitRepository
+// we're looking for errors.
+func NewGitRepository(upstream string) (CommonRepositoryBehaviour, error) {
+
+	if upstream == "" {
+		return nil, ErrInvalidUpstream{}
+	}
+
+	stat, err := os.Stat(upstream)
+
+	if err != nil {
+		//It's a remote upstream, not a local directory
+		if os.IsNotExist(err) {
+			// Assuming it's a remote upstream if the path doesn't exist
+			return NewGitHubRepository(upstream)
+		}
+	}
+
+	if stat.IsDir() {
+		return NewLocalGitRepository(upstream)
+	}
+
+	return nil, fmt.Errorf("unhandled scenario for upstream: %s", upstream)
+}
+
+func NewGitHubRepository(upstream string) (CommonRepositoryBehaviour, error) {
+	if upstream == "" {
+		return nil, ErrInvalidUpstream{}
+	}
+
+	repo := Repository{
+		Upstream: upstream,
+		ValidUpstreams: []string{
+			// Regular expression pattern for GitHub repository extraction
+			`(.*)[:/]([^/]+)/([^/.]+)(\.git)?$`,        // git@github.com:<owner>/<repo> or https://github.com/<owner>/<repo>
+			`https?://(.*)/([^/]+)/([^/.]+)(\.git)?$`,  // https://github.com/<owner>/<repo>
+			`git://(.*)/([^/]+)/([^/.]+)(\.git)?$`,     // git://github.com/<owner>/<repo>
+			`ssh://git@(.*)/([^/]+)/([^/.]+)(\.git)?$`, // ssh://git@github.com/<owner>/<repo>
+			`git@(.*):([^/]+)/([^/.]+)(\.git)?$`,       // git@github.com:<owner>/<repo>.git
+		},
+	}
+
+	var username_reponame string
+
+	for _, pattern := range repo.ValidUpstreams {
+		re := regexp.MustCompile(pattern)
+		matches := re.FindStringSubmatch(repo.Upstream)
+		if matches != nil {
+			// Return the repository name
+			username_reponame = matches[2] + "/" + matches[3]
+		}
+	}
+
+	//Need to extract the owner and repo from above
+	t := configelements.NewTemplDir()
+	repo.Destination = fmt.Sprintf("%s/github/%s", t.TemplatesDir, username_reponame)
+
+	return repo, nil
+}
+
+func NewLocalGitRepository(upstream string) (CommonRepositoryBehaviour, error) {
+	if upstream == "" {
+		return nil, ErrInvalidUpstream{}
+	}
+
+	stat, err := os.Stat(upstream)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !stat.IsDir() {
+		return nil, ErrNotDirectory{}
+	}
+
+	repo, err := &Repository{
+		Upstream: upstream,
+		ValidUpstreams: []string{
+			`(.*)`,
+		}}, nil
+
+	err = repo.validateUpstream()
+
+	if err != nil {
+		return nil, err
+	}
+
+	t := configelements.NewTemplDir()
+	repo.Destination = fmt.Sprintf("%s/local/%s", t.TemplatesDir, path.Base(repo.Upstream))
+
+	return repo, nil
+}
+
+// Fetch implements the CommonRepositoryBehaviour interface for Repository.
+func (r Repository) Fetch() error {
+
+	err := fetch(r)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r Repository) Origin() string {
+	return r.Upstream
+}
+
+func (r Repository) TemplDestination() string {
+	return r.Destination
+}
+
+// Finally, some private functions
+func (r Repository) validateUpstream() error {
+	// Iterate through the patterns and attempt to match the URL
+	for _, pattern := range r.ValidUpstreams {
+		re := regexp.MustCompile(pattern)
+		if re.MatchString(r.Upstream) {
+			return nil
+		}
+	}
+
+	return ErrInvalidUpstream{}
+}
+
+// No need for this to be on a repository.
+func fetch(repo Repository) error {
+	return nil
+}
