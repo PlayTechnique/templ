f935e8d7f63994835431a3ef5620e1067608a31b
Gwyn <gwyn@playtechnique.io>
2023-09-06 07:13:13 -0600

    Improved help text.

---
+++ bin/cicd-integration.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -elx
+
+cd "${GITHUB_WORKSPACE}"
+
+templ repo https://github.com/playtechnique/templ_templates
+
+echo "Catting the fetch charts script..."
+templ fetch
+
+cat > release-from-tag-config.yaml << EOF
+---
+OutputBinary: "roflcopter"
+EOF
+
+echo "Rendering release-from-tag.yaml"
+OUTPUT=$(templ render templ_templates/github_workflows/go/release-from-tag.yaml=release-from-tag-config.yaml)
+
+echo "Validating render..."
+echo "${OUTPUT}" | grep "roflcopter"
--- main.go
+++ main.go
@@ -15,17 +15,28 @@ func main() {
 	// string flag called clone. Takes a url as an argument
 	url := flag.String("fetch", "", "clone a git repository from a url. Can be a github url or a local git repository.")
 	update := flag.Bool("update", false, "iterate over template repositories, calling git update.")
+
+	usage := fmt.Sprintf("%s <templatename || templatename=variablesfile.yaml> <flags>\n\n"+
+		"<templatename> can come in one of two forms. First is a template filename,"+
+		"second is a template filename plus a yaml variables file of key: value pairs which"+
+		"will populate the template file's variables. If no variables file is provided, the utility"+
+		"operates like 'cat' on the file, printing it to stdout.\n\n",
+		//"This utility can also be called in a pipeline as %s templatename | %s FOO=BAR BAM=BAS, for folks who"+
+		//"prefer not to have a variables file.\n\n"
+		filepath.Base(os.Args[0]), filepath.Base(os.Args[0]), filepath.Base(os.Args[0]))
+
+	flag.Usage = func() { fmt.Println(usage); flag.PrintDefaults(); return }
 	flag.Parse()
 
 	candidateTemplates := flag.Args()
 
 	createTemplDir()
-	// I use github exclusively right now, so this is a safe bet. If I need to support more version control systems
-	// I'll need to implement a case statement and switch on some piece of data to figure out the difference between
-	// repo types. Probably need an extra cli flag.
 
 	// If the user has provided a url, clone the repo
 	if *url != "" {
+		// I use github exclusively right now, so this is a safe bet. If I need to support more version control systems
+		// I'll need to implement a case statement and switch on some piece of data to figure out the difference between
+		// repo types. Probably need an extra cli flag.
 		templRepo, err := repository.NewGitRepository(*url)
 
 		if err != nil {
