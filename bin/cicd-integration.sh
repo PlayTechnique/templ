aaaf3885de734dffbcc74d05828b2cc163eb506e
Gwyn <gwyn@playtechnique.io>
2023-09-06 07:14:18 -0600

    cicd integration test tweaks to get it working with the new syntax.

--- bin/cicd-integration.sh
+++ bin/cicd-integration.sh
@@ -2,7 +2,7 @@
 
 cd "${GITHUB_WORKSPACE}"
 
-templ repo https://github.com/playtechnique/templ_templates
+templ -fetch https://github.com/playtechnique/templ_templates
 
 echo "Catting the fetch charts script..."
 templ fetch
@@ -13,7 +13,7 @@ OutputBinary: "roflcopter"
 EOF
 
 echo "Rendering release-from-tag.yaml"
-OUTPUT=$(templ render templ_templates/github_workflows/go/release-from-tag.yaml=release-from-tag-config.yaml)
+OUTPUT=$(templ templ_templates/github_workflows/go/release-from-tag.yaml=release-from-tag-config.yaml)
 
 echo "Validating render..."
 echo "${OUTPUT}" | grep "roflcopter"
